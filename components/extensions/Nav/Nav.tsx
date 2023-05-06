cf2d3a00d7cc3782cea7cfbeb7cc40ca1672d7bb
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-05-06 04:25:10 +0100

    Changed Hamburger Icon

--- components/extensions/Nav/Nav.tsx
+++ components/extensions/Nav/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
       <nav className=" w-full  h-20 px-5 flex justify-between items-center z-[20] md:hidden ">
         <Logo />
         <div className="lg:hidden flex flex-col " >
-          <FaBars color={'#231F20'} className="text-[50px]" />
+          <FaBars color={'#231F20'} className="text-[40px]" />
         </div>
       </nav>
       {/* <nav></nav> */}
