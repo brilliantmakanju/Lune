a1d44cf2b500d1c46575ec9ee1d0802335c18a3e
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-05-08 09:47:09 +0100

    Edidted Login Model

--- components/extensions/Nav/CTA.tsx
+++ components/extensions/Nav/CTA.tsx
@@ -1,11 +1,25 @@
-import Link from "next/link"
+import useLoginModal from "@/components/models/hooks/useLoginModal";
+import Link from "next/link";
+import { useCallback } from "react";
 
 const CTA = () => {
+  const loginModal = useLoginModal();
+
+  const onClicks = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+
   return (
     <div className="lg:flex justify-end items-center hidden">
-      <Link className="flex justify-center items-center bg-white text-[black] py-2 px-7 rounded-full font-bold " href="/" >Log in</Link>
+      <button
+        onClick={() => onClicks()}
+        className="flex justify-center items-center bg-white text-[black] py-2 px-7 rounded-full font-bold "
+        // href="/"
+      >
+        Log in
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
