205f301ab9a0044abca99d59b8b46aea6d474e22
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-05-10 16:49:27 +0100

    Added Contact , About, Login, Register and Pricing page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -16,27 +16,27 @@ const Contact = () => {
             name=""
             id=""
             placeholder="Name"
-            className="h-[3em] bg-transparent  px-5 rounded-full pl-3 w-[100%] border-2 border-[#A30000] placeholder:text-gray-800 placeholder:font-bold text-gray-800 font-bold "
+            className="h-[3em] bg-transparent  px-5 rounded-full pl-3 w-[100%] border-2 lg:border-[#A30000] border-[black]  placeholder:text-gray-800 placeholder:font-bold text-gray-800 font-bold "
           />
           <input
             type="text"
             name=""
             id=""
             placeholder="Email"
-            className="h-[3em] bg-transparent  px-5 pl-3 w-[100%] border-2 border-[#A30000] rounded-full  placeholder:text-gray-800 placeholder:font-bold text-gray-800 font-bold "
+            className="h-[3em] bg-transparent  px-5 pl-3 w-[100%] border-2 lg:border-[#A30000] border-[black]  rounded-full  placeholder:text-gray-800 placeholder:font-bold text-gray-800 font-bold "
           />
           <input
             type="text"
             name=""
             id=""
             placeholder="Subject"
-            className="h-[3em] rounded-full bg-transparent  px-5 pl-3 w-[100%] border-2 border-[#A30000] placeholder:text-gray-800 placeholder:font-bold text-gray-800 font-bold "
+            className="h-[3em] rounded-full bg-transparent  px-5 pl-3 w-[100%] border-2 lg:border-[#A30000] border-[black]  placeholder:text-gray-800 placeholder:font-bold text-gray-800 font-bold "
           />
           <textarea
             placeholder="Message"
-            className="h-[12.5em] border-2 border-[#A30000]  bg-transparent w-[100%] placeholder:text-gray-800 placeholder:font-bold text-gray-800 font-bold rounded-[20px] pt-[10px] px-[10px] overflow-hidden "
+            className="h-[12.5em] border-2 lg:border-[#A30000] border-[black]   bg-transparent w-[100%] placeholder:text-gray-800 placeholder:font-bold text-gray-800 font-bold rounded-[20px] pt-[10px] px-[10px] overflow-hidden "
           ></textarea>
-          <button className="py-4 px-10 border-2 border-[#141313e7] ">
+          <button className="py-4 px-10 border-2 bg-[black] text-[white] border-[#141313e7] ">
             Submit
           </button>
         </form>
