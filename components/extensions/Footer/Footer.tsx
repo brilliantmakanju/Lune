eb00ffa65177b9c8b559bd182bb88abc51b2854b
Brilliant Makanju <brilliantmakanju5@gmail.com>
2023-05-11 20:45:48 +0100

    Added Font style

--- components/extensions/Footer/Footer.tsx
+++ components/extensions/Footer/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
         delay: 0.4,
         duration: 0.75,
       }}
-      className="z-[20] flex flex-col flex-wrap tracking-tighter gap-5 justify-start  items-start pt-[10px] pb-[50px] text-left place-content-start px-[20px] relative font-bold lg:px-[100px] lg:gap-[50px] w-screen xl:justify-center xl:items-center 2xl:py-10 bg-[#722f37] text-white 3xl:px-[150em] "
+      className="z-[20] flex flex-col flex-wrap tracking-tighter gap-5 justify-start  items-start pt-[10px] pb-[50px] text-left place-content-start px-[20px] relative font-bold lg:px-[100px] lg:gap-[50px] w-screen xl:justify-center xl:items-center 2xl:py-10 bg-[#722f37] text-white 3xl:px-[150em]  lg:mt-[30px] "
     >
       <div className="grid grid-cols-3 gap-10  lg:flex lg:flex-wrap lg:gap-[25px] ">
         <Legal />
